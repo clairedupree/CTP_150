daf683ee1afdd3c989fa77c1883b87dc6d068c0c
Claire Dupree <claire.e.dupree@gmail.com>
2022-02-20 15:07:49 -0500

    Added documentation, cleaned up formatting.

--- Module 1/Assignment1/TabSplitter.java
+++ Module 1/Assignment1/TabSplitter.java
@@ -1,78 +1,83 @@
-/** 
-*  This is a program which...
-*  @author Claire Dupree 02/18/22
-*
-*/
-
-/*
-Write a program that determines each person’s share of an equally split tab. 
-Using descriptive prompts, 
-
--the program should request the tab total, 
--the percentage tip to add, the tax on the tab, 
--and the number of people splitting the tab. 
-
--The program should calculate and display the total with tip, tax
-- and the total for each person using a descriptive display.  
-
-* You may assume that only numeric input will be supplied by the user.  
--Convert the final total to Pesos.  
-
-* Use a constant for the conversion amount.  
-* Please format your output to 2 decimal places. 
-
-1 United States Dollar equals
-20.58 Mexican Peso
-*/
-
-//TODO: Assess program organization, draw flowchart, decide if methods are necessary, which methods?
-
-
 //Import java scanner & decimal format libraries
 import java.util.Scanner;
 import java.text.DecimalFormat;
 
 
+/** 
+   CTP 150-400 Lab 1, Assignment 1:
+   
+   This program is a restaurant tab-splitter and currency converter.
+   The program requests user input for amounts of a restaurant tab in USD
+   and the number of people splitting the tab, and calculates the total 
+   amount owed by each person, in both US Dollars (USD) and Mexican Pesos.
+   
+   @author Claire Dupree 2/18/22
+*/
 public class TabSplitter
 {
-   //Declare global constants
+
+
+   /**
+      Current conversion rate for USD to Pesos, as of 2/18/22.
+   */
    static final double PESO_CONVERSION = 20.58;
 
+
+   /**
+      The main() method calls the intro() method, collects end-user 
+      input and sends that input to the calcTotal() method.
+   */
    public static void main(String[] args)
    {
       //Declare local variables
       double subtotal, tax, tip, total = 0.0;
       int numPeople = 0;
+           
             
       //Create Scanner object
       Scanner keyboard = new Scanner(System.in);
       
-      // Call program introduction method
+      
+      //Call method to introduce program
       intro();
       
-      //Request user input for tab total
+      
+      //Request user input for tab subtotal
       System.out.print("Please enter the tab subtotal: $");
       subtotal = keyboard.nextDouble();
       
+      
       //Request user input for tab tax
-      System.out.print("What is the tax percentage? (format as .00): ");
+      System.out.print("What percentage of tax was applied?" + 
+                       "(.06 for 6%): ");
+      //Calculate tax amount in USD, based on subtotal
       tax = (subtotal * keyboard.nextDouble());
                  
+                 
       //Request user input for tab tip
-      System.out.print("What percent tip would you like to leave? (format as .00): ");
+      System.out.print("What percent tip would you like to leave? " + 
+                       "(.20 for 20%): ");
+      //Calculate tip amount in USD, based on subtotal *including tax* 
       tip = ((subtotal + tax) * keyboard.nextDouble());
+
 
       //Request user input for number of people splitting tab
       System.out.print("How many people are splitting the tab?: ");
       numPeople = keyboard.nextInt();
       
-      //-The program should calculate and display the total with tip, tax
-      //- and the total for each person using a descriptive display.  
-      calcTotal(subtotal, tax, tip, numPeople);
-             
-   } //end main
+      
+      //Call method to calculate and display totals
+      calcTotal(subtotal, tax, tip, numPeople);    
+              
+   } //End main
    
-   //TODO: javadoc comment describing m,odule, use apporproate tags
+   
+   
+   
+   /**
+      The intro() method displays a visually-appealing header and 
+      introduces the purpose of the program to the end-user.
+   */
    public static void intro()
    {
       System.out.println("\t\t-- WELCOME TO THE TAB SPLITTER --");
@@ -81,41 +86,51 @@ public class TabSplitter
       System.out.println("in US Dollars and Pesos, and split it per person.");
       System.out.println("(No more arguing about who owes who money!)");
       System.out.println();
-   }
+   } //End intro()
    
    
-   public static void calcTotal(double subtotal, double tax, double tip, int numPeople)
-   {
-      //Calculate subtotal
-      //TODO: create and implement decimal formatting object
+   
+   
+   /**
+      The calcTotal() method recieves end-user input data from the main() method, 
+      calculates a total in USD, converts that total to Pesos, and displays 
+      itemized calculation data in a table.
+      
+      @param subtotal Tab subtotal amount in USD.
+      @param tax Tab tax amount in USD.
+      @param tip Tab tip amount in USD.
+      @param numPeople The number of people splitting the tab.
+   */
+   public static void calcTotal(double subtotal, double tax, double tip, 
+                                int numPeople)
+   {      
+      //Declare local variables
       double total = (subtotal + tax + tip);
       double totalPerPerson = (total / numPeople);
       
+      //Initialize decimal formatter object
+      DecimalFormat decimal = new DecimalFormat("#0.00");
+      
+      //Display final calculations to end-user in USD and Pesos
       System.out.println();
-      System.out.println("Subtotal:" + "\t\t\t\t" + "$" + subtotal);
-      System.out.println("Tax:" + "\t\t\t\t\t\t" + "$" + tax);
-      System.out.println("Tip:" + "\t\t\t\t\t\t" + "$" + tip);
-      System.out.println("-----------------------------");
-      System.out.println("Total (USD):" + "\t\t\t" + "$" + total);
-      System.out.println("Total (Pesos):" + "\t\t\t" + "$" + (total * PESO_CONVERSION));
-      System.out.println("-----------------------------");
-      System.out.println("Per person (USD):" + "\t\t" + "$" + totalPerPerson);
-      System.out.println("Per person (Pesos):" + "\t" + "$" + (totalPerPerson * PESO_CONVERSION));
-      
-      
-   } //End calcTotal
-      
-   /*
-   public static double getTabTotal()
-   {
-      Scanner keyboard = new Scanner(System.in);
-
-      System.out.print("Please enter the tab total: ");
-      double tabTotal = keyboard.nextDouble();
-      return tabTotal;
-      
-   } //end getTotal
-   */
+      System.out.println("Subtotal:" + "\t\t\t\t\t" + "$" + 
+                          decimal.format(subtotal));
+      System.out.println("Tax:" + "\t\t\t\t\t\t\t" + "$" + 
+                          decimal.format(tax));
+      System.out.println("Tip:" + "\t\t\t\t\t\t\t" + "$" + 
+                          decimal.format(tip));
+      System.out.println("----------------------------------");
+      System.out.println("Total (USD):" + "\t\t\t\t" + "$" + 
+                          decimal.format(total));
+      System.out.println("Total (Pesos):" + "\t\t\t\t" + "$" + 
+                          decimal.format((total * PESO_CONVERSION)));
+      System.out.println("----------------------------------");
+      System.out.println("Per person (USD):" + "\t\t\t" + "$" + 
+                          decimal.format(totalPerPerson));
+      System.out.println("Per person (Pesos):" + "\t\t" + "$" + 
+                          decimal.format((totalPerPerson * PESO_CONVERSION)));
    
-} //end TabSplitter class
-
+   } //End calcTotal()
+          
+      
+} //End TabSplitter
