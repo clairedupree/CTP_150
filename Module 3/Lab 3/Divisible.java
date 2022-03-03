70d2f99c1781a2b592cf103d680becf3db1fcb2e
Claire Dupree <claire.e.dupree@gmail.com>
2022-03-02 19:37:09 -0500

    Initial commit

---
+++ Module 3/Lab 3/Divisible.java
@@ -0,0 +1,43 @@
+//Imports
+import java.io.*;
+import java.util.Scanner;
+
+/**
+   Read a file that shows an upper bound on each record.
+   
+   1. Write a program that reads a record in the file and 
+   2. Shows integers in a range from 1 to that number (inclusive)that are divisible by 2
+   3. Show the sum of those integers
+   4. Show the number of integers not divisible by 2
+   5. Show the sum of those integers
+   6. Repeat all of the above, divisible by 3
+   7. Repeat for every line of the file.
+   
+   * Sample output: *
+   
+      The following results are based on the the first record in the file which is:  10
+         There are 5 numbers divisible by 2. The sum of the numbers divisible by 2 is 30.  Those numbers are 2 4 6 8 10
+         There are 5 numbers not divisible by 2. The sum of the numbers not divisible by 2 is 25.  Those numbers are 1 3 5 7 9 
+         
+         The following results are based on the the first record in the file which is:  10
+         There are 3 numbers divisible by 3. The sum of the numbers divisible by 3 is 18.  Those numbers are 3,6, 9.
+         There are 7 numbers not divisible by 3. The sum of the numbers not divisible by 3 is 37.  Those numbers are 1 2 4 5 7 8 10
+         
+   // Probably nested loops      
+   // TODO: introduce program.
+   // TODO: would you like to read the next record in the file?
+   // TODO: You have reached the end of the file.
+    
+   Read the next record and do the same thing as above 
+   
+   @author Claire Dupree
+   @since 03/02/22
+*/
+
+public class Divisible
+{
+   public static void main(String[] args)
+   {
+   
+   } //end main
+} //end class
