9855777ceb72cc0e9ebf39ce2d0431686667f587
Claire Dupree <claire.e.dupree@gmail.com>
2022-03-02 23:05:24 -0500

    Created import file, created loops
    Imported "divisible" file to utilize in program, began organizing program logic to solve provided prompt.

--- Module 3/Lab 3/Divisible.java
+++ Module 3/Lab 3/Divisible.java
@@ -36,8 +36,112 @@ import java.util.Scanner;
 
 public class Divisible
 {
-   public static void main(String[] args)
+   public static void main(String[] args) throws IOException
    {
-   
+      // Declare local variables
+      int fileNum = 0;
+      int numDivisible = 0;
+      String answer = "Y";
+      int runningTotal = 0;
+      int thisNum = 0;
+      int innerNum = 0;
+      //final int DIVIDE_BY_1ST = 2;
+      //final int DIVIDE_BY_2ND = 3;
+      
+      // Open file
+      File file = new File("divisible");
+      // Scanner file read object
+      Scanner inputFile = new Scanner(file);
+      // Scanner system input object
+      Scanner keyboard = new Scanner(System.in);
+
+
+      // Read first number from file
+      while (inputFile.hasNext() && (answer.equals("Y") || answer.equals("y"))) // " ?
+      {
+         fileNum = inputFile.nextInt();
+         System.out.println("The current number is: " + fileNum); //TODO: add punctuation?
+         //Calculate number of integers from 1-currentNum divisible by 2
+         
+         // How to efficently reuse the same loop for /2 & /3?
+         // for loop dividend=2; dividend <= 3
+         for(int dividend = 2; dividend <= 3; dividend++) //TODO: replace literals with constant?
+         {
+            // make this a method??? code reuse?
+            numDivisible = (int)(fileNum / dividend);
+            System.out.println("There are " + (numDivisible) + " numbers divisible by " + dividend); // TODO: replace with a constant?
+            System.out.print("These numbers are: ");
+            for(thisNum = 1; thisNum <= fileNum; thisNum++)
+            {
+               if ((thisNum % dividend) == 0)
+               {
+                  System.out.print(thisNum + " ");
+                  runningTotal += thisNum;
+               }
+
+            } 
+                     
+            System.out.println();
+            System.out.println("The sum of these numbers is: " + runningTotal);
+            System.out.println();
+            
+            //---------------repetitive code-------------------//
+            System.out.println("There are " + (fileNum-numDivisible) + " numbers not divisible by " + dividend);
+            System.out.print("These numbers are: ");
+            for(thisNum = 1; thisNum <= fileNum; thisNum++)
+            {
+               if ((thisNum % dividend) != 0)
+               {
+                  System.out.print(thisNum + " ");
+                  runningTotal += thisNum;
+               }   
+            } 
+                     
+            System.out.println();
+            System.out.println("The sum of these numbers is: " + runningTotal);
+            System.out.println();
+
+                         
+         }
+                  
+         // Continue?
+         System.out.print("Would you like to read another number? Y/N: ");
+         answer = keyboard.nextLine();
+         System.out.println();
+
+      }
+      
+      //inputFile.nextLine();
+      //keyboard.nextLine();
+      //if (inputFile.hasNext())
+         System.out.println("Goodbye");
+      //else
+         //System.out.println("That is the end of the file.");
+         //System.out.println("Goodbye.");
+         
+      // Close the file
+      inputFile.close();    
+      
+      
    } //end main
+   
+   /*
+   // Overengineering?
+   public static void numDivisible(int fileNum, int dividend, String type)
+         System.out.println("There are " + (fileNum / dividend) + " numbers " + type + " by " + dividend); // TODO: replace with a constant?
+         System.out.print("These numbers are: ");
+         for(thisNum = 1; thisNum <= fileNum; thisNum++)
+         {
+            if ((thisNum % dividend) == 0)
+            {
+               System.out.print(thisNum + " ");
+               runningTotal += thisNum;
+            }   
+         } 
+                  
+         System.out.println();
+         System.out.println("The sum of these numbers is: " + runningTotal);
+         System.out.println();
+   */
+
 } //end class
