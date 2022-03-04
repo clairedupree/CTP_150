7e837a28e82fe0ccc475ba5958c98a0db6476a2e
Claire Dupree <claire.e.dupree@gmail.com>
2022-03-04 18:50:29 -0500

    Update Divisible.java
    Separated the divideBy method into two, with each calculating numbers either divisible or not divisible. Cleaned up documentation.

--- Module 3/Lab 3/Divisible.java
+++ Module 3/Lab 3/Divisible.java
@@ -1,66 +1,75 @@
-//Imports
+// Import Java API libraries
 import java.io.*;
 import java.util.Scanner;
 
 /**
-   Read a file that shows an upper bound on each record.
-   
-   1. Write a program that reads a record in the file and 
-   2. Shows integers in a range from 1 to that number (inclusive)that are divisible by 2
-   3. Show the sum of those integers
-   4. Show the number of integers not divisible by 2
-   5. Show the sum of those integers
-   6. Repeat all of the above, divisible by 3
-   7. Repeat for every line of the file.
-   
-   * Sample output: *
-   
-      The following results are based on the the first record in the file which is:  10
-         There are 5 numbers divisible by 2. The sum of the numbers divisible by 2 is 30.  Those numbers are 2 4 6 8 10
-         There are 5 numbers not divisible by 2. The sum of the numbers not divisible by 2 is 25.  Those numbers are 1 3 5 7 9 
-         
-         The following results are based on the the first record in the file which is:  10
-         There are 3 numbers divisible by 3. The sum of the numbers divisible by 3 is 18.  Those numbers are 3,6, 9.
-         There are 7 numbers not divisible by 3. The sum of the numbers not divisible by 3 is 37.  Those numbers are 1 2 4 5 7 8 10
-         
-   // Probably nested loops      
+   CTP 150-400 | Module 3 | Lab 3
+   <br>
+   This program performs a series of division operations on integers 
+   imported from a file. Using a loop, the program imports each file 
+   line, determines which numbers from 1 to that integer are divisible 
+   by a chosen dividend, and then displays those numbers and the sum of 
+   those numbers. The program then does the same for numbers which are 
+   not divisible by the chosen dividend. 
+        
    // TODO: introduce program.
-   // TODO: would you like to read the next record in the file?
    // TODO: You have reached the end of the file.
-    
-   Read the next record and do the same thing as above 
-   
+   // TODO: less comments
+   // TODO: more spacing
+       
    @author Claire Dupree
-   @since 03/02/22
+   @since 03/04/22
 */
 
 public class Divisible
 {
+   
+   /**
+      Main module. Imports a file, calls intro(), reads each line from 
+      that file and sends as an argument to divisibleBy() or 
+      notDivisibleBy() on a loop.
+   */
+   
    public static void main(String[] args) throws IOException
    {
       // Declare local variables
       int fileNum = 0;
-      String keepGoing = "Y";
+      String keepGoing = "Y"; // Initialized to "yes"
        
       // Open file
       File file = new File("divisible");
-      // Scanner for file reads
+      
+      // Create scanner for file reads
       Scanner inputFile = new Scanner(file);
-      // Scanner for user input
+      // Create scanner for user input
       Scanner keyboard = new Scanner(System.in);
       
+      // Call module to introduce program
+      intro();
       
-      while (inputFile.hasNext() && (keepGoing.equals("Y") || keepGoing.equals("y")))
+      // Ask end-user if they want to start
+      System.out.print("Would you like to calculate the first number? Y/N: ");
+      keepGoing = keyboard.nextLine();
+      System.out.println();
+      
+      // Enter loop if the file has another line 
+      // and when user selects continue
+      while (inputFile.hasNext() && (keepGoing.equals("Y") 
+                                 || keepGoing.equals("y")))
       {
+         // Import next integer
          fileNum = inputFile.nextInt();
          System.out.println("Current num: " + fileNum);
          
-         //call divide by module, send 2 as dividend
-         divideBy(fileNum, 2);
+         // Call division modules, 2 as dividend
+         divisibleBy(fileNum, 2);
+         notDivisibleBy(fileNum, 2);
          
-         //call divide by module, send 3 as dividend
-         divideBy(fileNum, 3);
+         // Call division modules, 3 as dividend
+         divisibleBy(fileNum, 3);
+         notDivisibleBy(fileNum, 3);
          
+         // Ask end-user if they want to continue
          System.out.print("Would you like to read another number? Y/N: ");
          keepGoing = keyboard.nextLine();
          System.out.println();
@@ -75,61 +84,115 @@ public class Divisible
       
    } //end main
    
-   public static void divideBy(int maxNum, int dividend)
+   
+   
+   
+   /**
+      Introduces program to end-user.
+   */
+   public static void intro()
+   {
+      //INSERT INTRO
+      System.out.println("\t\t\t-- IS IT DIVISIBLE? --");
+      System.out.println("This program reads integers from a file and");
+      System.out.println("displays which ones are divisible and which");
+      System.out.println("ones are not divisible by a chosen dividend.");
+      System.out.println();
+   }
+   
+   
+   
+      
+   /**
+      Accepts an integer and a dividend and calculates how many numbers
+      from 1 to that integer that are divisible by the chosen dividend.
+      Calculates the sum of these numbers and displays the results of
+      all calculations in a table.
+      
+      @param maxNum Integer to be the high-end of the range of divisible numbers
+      @param dividend Integer which each number be divided by
+   */
+   
+   public static void divisibleBy(int maxNum, int dividend)
    {
       // Declare local sum
       int sum = 0;
       
-      // Calculate numbers divisible by dividend
+      // Calculate & display the number of integers divisible by dividend
       System.out.print((maxNum / dividend) + " numbers are divisible by " + dividend + ": ");
       
+      // Loop through each integer starting at 1 - file number
       for(int currentNum = 1; currentNum <= maxNum; currentNum++)
       {
+         // Display each integer divisible by dividend      
          if((currentNum % dividend) == 0)
          {
             System.out.print(currentNum + " ");
-            sum += currentNum;
+            sum += currentNum; // Add integer to sum
          } //end if
       } //end for
       
+      // Display sum of divisible integers
       System.out.println();
       System.out.println("Sum: " + sum);
+   } // end divisibleBy
+
+
+
+
+   /**
+      Accepts an integer and a dividend and calculates how many numbers
+      from 1 to that integer that NOT are divisible by the chosen dividend.
+      Calculates the sum of these numbers and displays the results of
+      all calculations in a table.
       
-      sum = 0; //clear sum
+      @param maxNum Integer to be the high-end of the range of non-divisible numbers
+      @param dividend Integer which each number be divided by
+   */
+   
+   public static void notDivisibleBy(int maxNum, int dividend)
+   {      
+      // Declare local sum
+      int sum = 0;
       
-      // Calculate numbers not divisible by dividend
+      // Calculate & display the number of integers NOT divisible by dividend
       System.out.print((maxNum - (maxNum / dividend)) + " numbers not divisible by " + dividend + ": ");
       for(int currentNum = 1; currentNum <= maxNum; currentNum++)
       {
+         // Loop through each integer starting at 1 - file number
          if((currentNum % dividend) != 0)
          {
+            // Display each integer NOT divisible by dividend      
             System.out.print(currentNum + " ");
-            sum += currentNum;
+            sum += currentNum; // Add integer to sum
          } //end if
       } //end for
       
+      // Display sum of non-divisible integers
       System.out.println();
       System.out.println("Sum: " + sum);
 
-
-   } //end divideBy
+   } //end notDivisibleBy
 
 } //end class
 
 
       /*
-      // POTENTIAL SOLUTION, nested loops:
-      // Logic works but is not ideal, code is confusing, heavy indentation, not scalable with different dividends
+      // Alternative solution using nested loops:
+      // Logic works but is not ideal, confusing logic,
+      // heavy indentation, not scalable with different dividends
       // Would need to declare int sum and int dividend in code body
       
-      while (inputFile.hasNext() && (keepGoing.equals("Y") || keepGoing.equals("y")))
+      while (inputFile.hasNext() && (keepGoing.equals("Y") 
+                                 || keepGoing.equals("y")))
       {
          fileNum = inputFile.nextInt();
          System.out.println("Current num: " + fileNum);
          
          for(dividend = 2; dividend <= 3; dividend++)
          {
-            System.out.print((fileNum / dividend) + " numbers divisible by " + dividend + ": ");
+            System.out.print((fileNum / dividend) + " numbers divisible by " 
+                                                  + dividend + ": ");
             sum = 0; //clear sum
             for(int currentNum = 1; currentNum <= fileNum; currentNum++)
             {
@@ -144,7 +207,8 @@ public class Divisible
             
             
             
-            System.out.print((fileNum - (fileNum / dividend)) + " numbers not divisible by " + dividend + ": ");
+            System.out.print((fileNum - (fileNum / dividend)) 
+                              + " numbers not divisible by " + dividend + ": ");
             sum = 0; //clear sum
             for(int currentNum = 1; currentNum <= fileNum; currentNum++)
             {
