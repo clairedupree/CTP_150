c5cd33468da33413f087148fac645b341bad2d00
Claire Dupree <claire.e.dupree@gmail.com>
2022-03-27 18:07:39 -0400

    Added displayPlayers() and displayPlayersReverse() methods

--- Module 5/Lab 5/Assignment 2/Football.java
+++ Module 5/Lab 5/Assignment 2/Football.java
@@ -50,6 +50,38 @@ public class Football
       read.close();
    }
    
+   
+   /**
+      add docs
+   */   
+   public void displayPlayers()
+   {
+      System.out.println("Players from highest to lowest rank:");
+      System.out.println("------------------------------------");
+      for (int i = 0; i < players.length; i++)
+      {
+         System.out.printf("#%d:\t%s\n", i + 1, players[i]);
+      }
+   }
+   
+ 
+   /**
+      add docs
+   */   
+   public void displayPlayersReverse()
+   {
+      System.out.println("Players from lowest to highest rank:");
+      System.out.println("------------------------------------");
+      for (int i = players.length; i > 0; i--)
+      {
+         System.out.printf("#%d:\t%s\n", i, players[i-1]);
+      }
+   }
+
+   
+   /**
+      add docs
+   */
    public void displayWinnersByNumber()
    {
       int select = 1; //to store user input
@@ -65,20 +97,30 @@ public class Football
          }
          
          // Display selected player based on rank
-         System.out.printf("Rank %d: %s\n", select, players[select - 1]);
+         // -1 to account for correct array subscript number
+         System.out.printf("#%d: %s\n", select, players[select - 1]);
          System.out.println(); //line break
-
+         
+         // Ask user if they choose to search another rank
          System.out.println("Search another rank?\n1) Yes\n2) No");
          select = input.nextInt();
-            while (select != 1 && select != 2)
+         System.out.println(); //line break
+
+            // error checking
+            while (select != 1 && select != 2) //is not valid
             {
-               System.out.println("Invalid entry. Please select either:\n1) Yes\n2) No");
+               // Display error, prompt user to try again
+               System.out.println("Invalid entry. Please select:\n1) Yes\n2) No");
                select = input.nextInt();
-            }
-               
+               System.out.println(); //line break
+            }              
       } while (select == 1);
    }
    
+   
+   /**
+      add docs
+   */   
    public static void exit()
    {
       System.out.println("Goodbye");
