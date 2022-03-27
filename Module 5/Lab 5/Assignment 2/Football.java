35cf9b907dae8bfceae24ba64649238da5f918a2
Claire Dupree <claire.e.dupree@gmail.com>
2022-03-27 14:41:34 -0400

    Updated Football class and application driver

--- Module 5/Lab 5/Assignment 2/Football.java
+++ Module 5/Lab 5/Assignment 2/Football.java
@@ -1,26 +1,49 @@
+import java.io.*; //files
+import java.util.Scanner; //scanner class
+
 /*
 -- CLASS DEFINITION --
-
-Use the same file above but without the averages:  here Download here
-
-Develop a class named Football that stores Football players above in an array of Strings.
-The first player is ranked number 1 and Aaron Rogers is ranked number 10  Hence, the array must accommodate 10.
-The class constructor should read the FootballNames.txt file, storing each record read into an element of the array.  
-
-The Football class defines methods to support queries of the array.  The following methods should be defined:
-displayPlayers():  Displays all Players ranked 1 through 10.  The rank and name should be displayed for all data items.
-displayPlayersReverse():  Displays all Players ranked 10 through 1.  The rank and name should be displayed for all data items.
-displayWinnersByNumber():  Displays all Football players by number of rank.  The rank and name should be displayed for all data items.
-I type in a rank from 1 - 10 and display that player's name and rank. 
-findPlayers():  Searches the array for all occurrences of the ball club name given as a parameter.
-The method displays the rank and name of the player and  an appropriate message when not found. I type in Lamar Jackson.
-Is his name in the file?
-
-Exit(): Says goodbye
-Include a menu that will have these  options and choose one and run the program. 
 */
 
 public class Football
 {
+   /**
+      add documentation
+   */
+   private String[] players; // player names
    
+   /**
+      add docs
+   */
+   private final int NUM_PLAYERS = 10; // number of players on the source file
+   
+   
+   /**
+      This constructor...
+   */
+   public Football() throws IOException
+   {
+      // Open source file
+      File inputFile = new File("footballnames.txt");
+      if (!inputFile.exists()) // is not valid
+      {
+         System.out.println("File not found."); // display error message
+         System.exit(0); // close program
+      }
+      
+      // Create scanner to read source file
+      Scanner read = new Scanner(inputFile);
+      
+      // Initialize array to needed size for source file
+      players = new String[NUM_PLAYERS];
+      
+      // Clear file header
+      read.nextLine();
+      
+      for (int i = 0; i < players.length; i++)
+      {
+         players[i] = read.nextLine();  
+         //System.out.println(players[i]);
+      }
+   }
 }
