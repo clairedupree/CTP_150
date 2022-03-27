003647dfbb8eb725f9b06e7210db92243919596d
Claire Dupree <claire.e.dupree@gmail.com>
2022-03-27 17:23:31 -0400

    Created displayWinnersByNumber() method

--- Module 5/Lab 5/Assignment 2/Football.java
+++ Module 5/Lab 5/Assignment 2/Football.java
@@ -45,5 +45,42 @@ public class Football
          players[i] = read.nextLine();  
          //System.out.println(players[i]);
       }
+      
+      // close the file
+      read.close();
+   }
+   
+   public void displayWinnersByNumber()
+   {
+      int select = 1; //to store user input
+      Scanner input = new Scanner(System.in); //to collect user input
+      
+      do {
+         System.out.print("Please enter a rank number from 1-10: ");
+         select = input.nextInt();
+         while (select > 10 || select < 1)
+         {
+            System.out.print("Invalid entry, please enter a rank from 1-10: ");
+            select = input.nextInt(); 
+         }
+         
+         // Display selected player based on rank
+         System.out.printf("Rank %d: %s\n", select, players[select - 1]);
+         System.out.println(); //line break
+
+         System.out.println("Search another rank?\n1) Yes\n2) No");
+         select = input.nextInt();
+            while (select != 1 && select != 2)
+            {
+               System.out.println("Invalid entry. Please select either:\n1) Yes\n2) No");
+               select = input.nextInt();
+            }
+               
+      } while (select == 1);
+   }
+   
+   public static void exit()
+   {
+      System.out.println("Goodbye");
    }
 }
