f5b837b78d2dade663b2dfab183e46abda3eb7a4
Claire Dupree <claire.e.dupree@gmail.com>
2022-03-27 21:34:19 -0400

    Updated documentation

--- Module 5/Lab 5/Assignment 2/Football.java
+++ Module 5/Lab 5/Assignment 2/Football.java
@@ -1,25 +1,32 @@
-import java.io.*; //files
-import java.util.Scanner; //scanner class
+import java.io.*; 
+import java.util.Scanner; 
 
-/*
--- CLASS DEFINITION --
+/**
+   CTP 150-400 | Assignment no. 2
+   <br>
+   Class definition for the Football program.
+   @author Claire Dupree
+   @since 03/27/22
 */
 
 public class Football
 {
    /**
-      add documentation
+      An array of Strings to hold football player names, in order of rank.
    */
-   private String[] players; // player names
+   private String[] players;
    
    /**
-      add docs
+      A constant used to set the size of the players[] array, 
+      determined by the number of player names found in the source file.
    */
-   private final int NUM_PLAYERS = 10; // number of players on the source file
-   
+   private final int NUM_PLAYERS = 10; 
+      
    
    /**
-      This constructor...
+      This constructor opens a source file containing football player names
+      and imports those names into the players[] array.
+      @throws IOException for file use during runtime.
    */
    public Football() throws IOException
    {
@@ -34,30 +41,36 @@ public class Football
       // Create scanner to read source file
       Scanner read = new Scanner(inputFile);
       
-      // Initialize array to needed size for source file
+      // Initialize array to appropriate size for source file
       players = new String[NUM_PLAYERS];
       
       // Clear file header
       read.nextLine();
       
+      // Loop through each array element
       for (int i = 0; i < players.length; i++)
       {
+         // Populate array element with current file line
          players[i] = read.nextLine();  
          //System.out.println(players[i]);
       }
       
-      // close the file
+      // Close the file
       read.close();
    }
    
    
    /**
-      add docs
+      This method displays the names and ranks of the football players
+      from highest to lowest rank.
    */   
    public void displayPlayers()
    {
+      // Display header
       System.out.println("Players from highest to lowest rank:");
       System.out.println("------------------------------------");
+      
+      // Loop through and display each array element
       for (int i = 0; i < players.length; i++)
       {
          System.out.printf("#%d:\t%s\n", i + 1, players[i]);
@@ -66,12 +79,16 @@ public class Football
    
  
    /**
-      add docs
-   */   
+      This method displays the names and ranks of the football players
+      from lowest to highest rank.
+   */    
    public void displayPlayersReverse()
    {
+      // Display header
       System.out.println("Players from lowest to highest rank:");
       System.out.println("------------------------------------");
+      
+      // Loop through and display each array element
       for (int i = players.length; i > 0; i--)
       {
          System.out.printf("#%d:\t%s\n", i, players[i-1]);
@@ -80,18 +97,29 @@ public class Football
 
    
    /**
-      add docs
+      This method displays the name of a player associated with a
+      rank number input by the user.
    */
    public void displayWinnersByNumber()
    {
-      int select = 1; //to store user input
-      Scanner input = new Scanner(System.in); //to collect user input
+      //TODO: Move user input options to select case 3 in main() module?
+      //OR: Move select case 4 input options to findPlayers() module?
       
+      // Local variable for user input
+      int select = 1;
+      
+      // Initialize Scanner for user input
+      Scanner input = new Scanner(System.in); 
+      
+      // Continue search loop
       do {
+         // Prompt user to enter a rank number      
          System.out.print("Please enter a rank number from 1-10: ");
          select = input.nextInt();
-         while (select > 10 || select < 1)
+         
+         while (select > NUM_PLAYERS || select < 1) // is not valid
          {
+            // Prompt user to try again
             System.out.print("Invalid entry, please enter a rank from 1-10: ");
             select = input.nextInt(); 
          }
@@ -99,6 +127,7 @@ public class Football
          // Display selected player based on rank
          // -1 to account for correct array subscript number
          System.out.printf("#%d: %s\n", select, players[select - 1]);
+         
          System.out.println(); //line break
          
          // Ask user if they choose to search another rank
@@ -106,20 +135,51 @@ public class Football
          select = input.nextInt();
          System.out.println(); //line break
 
-            // error checking
-            while (select != 1 && select != 2) //is not valid
-            {
-               // Display error, prompt user to try again
-               System.out.println("Invalid entry. Please select:\n1) Yes\n2) No");
-               select = input.nextInt();
-               System.out.println(); //line break
-            }              
-      } while (select == 1);
+         while (select != 1 && select != 2) //is not valid
+         {
+            // Prompt user to try again
+            System.out.println("Invalid entry. Please select:\n1) Yes\n2) No");
+            select = input.nextInt();
+            
+            System.out.println(); //line break
+         }              
+      } while (select == 1); // Continue while user selects "Yes"
    }
    
    
    /**
-      add docs
+      This function accepts a search string, compares that string with
+      player names in the players[] array, and returns the rank of the 
+      searched name, if it is found within the list of names.
+      @param search The user input to be searched.
+      @return The subscript location of the name, if found, or -1 if not found.
+   */   
+   public int findPlayers(String search)
+   {
+
+      int i = 0; // Array index variable
+      boolean found = false; // Flag for found or not found
+      int location = -1; // Subscript location of the search results in the array
+      
+      // Continue searching while not found and while there are elements left
+      while (!found && i < players.length)
+      {
+         // If search matches an array element
+         if (players[i].equals(search))
+         {
+            // Found is true and index location is stored
+            found = true;
+            location = i;
+         }
+         i++;
+      }
+      // Return index location of match or -1 for no match     
+      return location;
+   }
+   
+   
+   /**
+      This method prompts the user that the program is ending.
    */   
    public static void exit()
    {
--- Module 5/Lab 5/Assignment 2/FootballDriver.java
+++ Module 5/Lab 5/Assignment 2/FootballDriver.java
@@ -1,18 +1,32 @@
 import java.io.*;
 import java.util.Scanner;
 
-/*
--- DRIVER --
+/**
+   CTP 150-400 | Assignment no. 2
+   <br>
+   Application driver for the object-oriented Football program.
+   @author Claire Dupree
+   @since 03/27/22
 */
-
 public class FootballDriver
 {
+   /**
+      The main() module contains the menu to operate the program
+      and calls methods from the Football class based on user selection.
+      @param args A String[] of args.
+      @throws IOException For file use during runtime.
+   */
    public static void main(String[] args) throws IOException
    {
       /**
-         documentation
+         A variable to hold user integer menu selection input.
       */
-      int select = 0; // to hold user menu input
+      int select = 0;
+      
+      /**
+         A variable to hold user string search input.
+      */      
+      String search = "";
       
       // Create object of football class
       Football fb = new Football();
@@ -20,36 +34,65 @@ public class FootballDriver
       // Create a scanner object to collect user input
       Scanner input = new Scanner(System.in);
       
-      // menu do while loop
+      // Display Main Menu
       do {
          System.out.println("\t\t\t\t== MAIN MENU ==");
          System.out.println("****************************************");
-         System.out.println("1) Show all players - ascending order");
-         System.out.println("2) Show all players - descending order");
+         System.out.println("1) Show all players in ascending order");
+         System.out.println("2) Show all players in descending order");
          System.out.println("3) Search a player by rank");
          System.out.println("4) Search a player by name");
          System.out.println("5) Exit");
          
+         // Collect user menu selection
          select = input.nextInt();
          System.out.println(); // line break
          
          switch(select)
          {
-            case 1: fb.displayPlayers();
+            // Call method to display ascending players
+            case 1: 
+               fb.displayPlayers();
             break;
-            case 2: fb.displayPlayersReverse();
+            
+            // Call method to display descending players
+            case 2: 
+               fb.displayPlayersReverse();
             break;
-            case 3: fb.displayWinnersByNumber();
+            
+            // Call method to search a player by number
+            case 3: 
+               fb.displayWinnersByNumber();
             break;
-            case 4:
+            
+            // Call method to search a player by name
+            case 4: 
+               input.nextLine(); // clear scanner
+               
+               // Collect user search input
+               System.out.print("Please enter the name of a player you would like to search: ");
+               search = input.nextLine();
+
+               // Display error if search is not found
+               if (fb.findPlayers(search) == -1)
+                  System.out.println("Player not found in list");
+                  // TODO: Add a "search another?" feature
+               // Display player rank if search is found
+               else
+                  System.out.printf("%s is ranked #%d\n", search, fb.findPlayers(search)+1);
+            break;
+            
+            // Display error if menu selection is invalid
+            default: 
+               if (select != 5)
+                  System.out.println("Invalid entry, try again");
          }
          
          System.out.println(); // line break
          
       } while (select != 5);
       
-      // exit method
+      // End program
       fb.exit();
-      
    }   
 }
